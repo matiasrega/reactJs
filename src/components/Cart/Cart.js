41c58a6f9638b5b758b24e0845670ebb8ee76692
Matias Rega <matias.rega@hotmail.com>
2023-04-17 00:15:52 -0300

    Agregando propiedades al carrrito

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useState } from "react";
 import { useCart } from "../CartContext/CartContext";
+/*import Item from "../items/Item";*/
 
 const Cart = () => {
   const { cartItems, updateCartItems } = useCart();
@@ -14,6 +15,8 @@ const Cart = () => {
     updateCartItems([]);
   };
 
+  const total = items.reduce((acc, item) => acc + item.importe, 0);
+
   return (
     <div>
       <h1>Carrito de compras</h1>
@@ -21,9 +24,13 @@ const Cart = () => {
         {items.map((item) => (
           <li key={item.id}>
             {item.nombre} - ${item.importe}
+            <img src={item.imagen} alt={item.nombre} />
           </li>
         ))}
       </ul>
+
+      <p>Total de la Compra: ${total}</p>
+
       <button onClick={handleEmptyCart}>Vaciar carrito</button>
     </div>
   );
